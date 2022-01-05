67b3df11d58420db39333f6e3eaffe3eea2ffa26
Gabriel Seals <sealsgj@gmail.com>
2022-01-05 17:02:19 -0600

    get one member at a time

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const app = express();
 // Gets All Members
 app.get('/api/members', (req, res) => res.json(members));
 
+// Get Single Member
+app.get('/api/member/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if(found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)))
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
